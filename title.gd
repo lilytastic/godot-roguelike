510d25c13ebcc4bb00ed58628d2dac0f6af85088
Lily <thebeaul@gmail.com>
2025-01-17 17:56:00 -0500

    start on title

--- title.gd
+++ title.gd
@@ -1,6 +1,6 @@
 extends Node2D
 
 func _unhandled_input(event: InputEvent) -> void:
-	if event.is_action_pressed:
+	if event is InputEventKey and event.pressed:
 		get_tree().change_scene_to_file('res://game/game.tscn')
 		return
