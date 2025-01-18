d52c824e72d9f62f52485c16f62d469cd8113d83
Lily <thebeaul@gmail.com>
2025-01-18 12:56:58 -0500

    load all resources during runtime!

--- game/game.gd
+++ game/game.gd
@@ -9,13 +9,19 @@ func _ready() -> void:
 	options.blueprint = 'hero'
 	var new_id = ECS.add(Entity.new(options))
 	print(new_id, ECS.entity(new_id))
-	var resource = preload("res://data/creatures.json").data
-	var blueprints = resource.data.blueprints
+	var resources = get_all_files('res://data')
 	var dict: Dictionary
-	for blueprint in blueprints:
-		dict[blueprint.get("id")] = Blueprint.new(blueprint)
+	for resource in resources:
+		print(resource)
+		var file_access = FileAccess.open(resource, FileAccess.READ)
+		var file = JSON.parse_string(file_access.get_as_text())
+		file_access.close()
+		var data = file.data if file.has('data') else {}
+		if data.has('blueprints'):
+			var blueprints = data.blueprints
+			for blueprint in blueprints:
+				dict[blueprint.get("id")] = Blueprint.new(blueprint)
 	print(dict.values().size(), " records loaded")
-	print(dict['hero'].glyph.ch)
 	_create_pc()
 	
 
@@ -52,3 +58,32 @@ func _move_pc(direction: StringName) -> void:
 			coord += Vector2i.DOWN
 
 	pc.position = Coords.get_position(coord)
+
+
+## returns list of files at given path recursively
+## [br]taken from - https://gist.github.com/hiulit/772b8784436898fd7f942750ad99e33e
+static func get_all_files(path: String, file_ext := "", files : Array[String] = []) -> Array[String]:
+	var dir : = DirAccess.open(path)
+	if file_ext.begins_with("."): # get rid of starting dot if we used, for example ".tscn" instead of "tscn"
+		file_ext = file_ext.substr(1,file_ext.length()-1)
+	
+	if DirAccess.get_open_error() == OK:
+		dir.list_dir_begin()
+
+		var file_name = dir.get_next()
+
+		while file_name != "":
+			if dir.current_is_dir():
+				# recursion
+				files = get_all_files(dir.get_current_dir() +"/"+ file_name, file_ext, files)
+			else:
+				if file_ext and file_name.get_extension() != file_ext:
+					file_name = dir.get_next()
+					continue
+				
+				files.append(dir.get_current_dir() +"/"+ file_name)
+
+			file_name = dir.get_next()
+	else:
+		print("[get_all_files()] An error occurred when trying to access %s." % path)
+	return files
