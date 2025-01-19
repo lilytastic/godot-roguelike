e7827c5adec520862c268ecfbe2c05695e0f3ef6
Lily <thebeaul@gmail.com>
2025-01-18 22:34:05 -0500

    ...

--- game/game.gd
+++ game/game.gd
@@ -1,20 +1,19 @@
 extends Node
 
 const PC_TAG = 'PC'
-var player: Entity
+var player: Entity:
+	get: return Global.player
 var cameraSpeed := 6
 
 
 func _ready() -> void:
-	RenderingServer.set_default_clear_color(Palette.PALETTE.BACKGROUND)
-	Global.ecs.load_data()
 	$TileMapLayer.map = 'Test'
 
-	_new_game()
+	Global._new_game()
 
 
 func _process(delta: float) -> void:
-	if player.position != null:
+	if player and player.position != null:
 		$Camera2D.position = lerp($Camera2D.position, Coords.get_position(player.position), delta * cameraSpeed)
 	$Camera2D.offset = Vector2i(8 + 16 * 0, 8)
 	
@@ -22,45 +21,9 @@ func _process(delta: float) -> void:
 func _unhandled_input(event: InputEvent) -> void:
 	for i: StringName in InputTag.MOVE_ACTIONS:
 		if event.is_action_pressed(i):
-			_move_pc(i)
+			Global._move_pc(i)
 			return
 	
 	if event.is_action_pressed('quicksave'):
-		save()
+		Global.save()
 		return
-
-func _new_game() -> void:
-	var options = EntityCreationOptions.new()
-	options.blueprint = 'hero'
-	player = Global.ecs.create(options)
-	player.map = 'Test'
-	# player.position = Coords.get_position(Vector2i(0, 0))
-	save()
-	
-
-func save() -> void:
-	var data = {}
-	data.entities = Global.ecs.entities.values().map(
-		func(entity): return entity.save()
-	)
-	data.player = player.uuid
-	Files.save(data)
-
-
-func _move_pc(direction: StringName) -> void:
-	if !player:
-		return
-
-	var coord: Vector2i = player.position
-	
-	match direction:
-		InputTag.MOVE_LEFT:
-			coord += Vector2i.LEFT
-		InputTag.MOVE_RIGHT:
-			coord += Vector2i.RIGHT
-		InputTag.MOVE_UP:
-			coord += Vector2i.UP
-		InputTag.MOVE_DOWN:
-			coord += Vector2i.DOWN
-
-	player.position = coord
--- global.gd
+++ global.gd
@@ -1,3 +1,45 @@
 extends Node
 
 var ecs = ECS.new()
+var player: Entity
+
+func _ready() -> void:
+	RenderingServer.set_default_clear_color(Palette.PALETTE.BACKGROUND)
+	Global.ecs.load_data()
+  
+
+func _new_game() -> void:
+	var options = EntityCreationOptions.new()
+	options.blueprint = 'hero'
+	player = Global.ecs.create(options)
+	player.map = 'Test'
+	# player.position = Coords.get_position(Vector2i(0, 0))
+	save()
+	
+
+func save() -> void:
+	var data = {}
+	data.entities = Global.ecs.entities.values().map(
+		func(entity): return entity.save()
+	)
+	data.player = player.uuid
+	Files.save(data)
+
+
+func _move_pc(direction: StringName) -> void:
+	if !player:
+		return
+
+	var coord: Vector2i = player.position
+	
+	match direction:
+		InputTag.MOVE_LEFT:
+			coord += Vector2i.LEFT
+		InputTag.MOVE_RIGHT:
+			coord += Vector2i.RIGHT
+		InputTag.MOVE_UP:
+			coord += Vector2i.UP
+		InputTag.MOVE_DOWN:
+			coord += Vector2i.DOWN
+
+	player.position = coord
