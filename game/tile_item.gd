4879bd8a8ce2c1b221b058da28fc20195c7d7213
Lily <thebeaul@gmail.com>
2025-01-21 18:42:27 -0500

    ...

--- game/tile_item.gd
+++ game/tile_item.gd
@@ -1,6 +1,6 @@
 extends Button
 
-var entity := Global.ecs.create({ 'blueprint': 'sword' })
+var entity: Entity # Global.ecs.create({ 'blueprint': 'sword' })
 
 func _get_drag_data(at_position: Vector2) -> Variant:
 	if entity:
@@ -14,4 +14,8 @@ func _can_drop_data(at_position: Vector2, data: Variant) -> bool:
 
 func _drop_data(at_position: Vector2, data: Variant) -> void:
 	print(data)
+	entity = Global.ecs.entity(data)
 	
+func _process(delta):
+	if self.icon and entity == null:
+		self.icon = null
