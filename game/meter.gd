1b4ae6eeb2b5076e41d5f3fdd7ec711f77c7d5fa
Lily <thebeaul@gmail.com>
2025-02-16 19:28:38 -0500

    oop

--- game/meter.gd
+++ game/meter.gd
@@ -1,11 +1,11 @@
 extends Panel
 
-@export var value = 0.5
+@export var value = 1.0
 @export var max = 1
 @export var label = ''
 
 func _process(delta: float) -> void:
-	var _scaled_value = value / max * size.y
+	var _scaled_value = float(value) / max * size.y
 	%Fill.position = %Fill.position.lerp(Vector2(0, size.y - _scaled_value), delta * 30.0)
 	%Fill.size = %Fill.size.lerp(Vector2(size.x, _scaled_value), delta * 30.0)
 	%Label.text = '[center]' + label + '[/center]'
