1960a6ad4425a92e2a3b7b517252b166b28fc520
Lily <thebeaul@gmail.com>
2025-02-23 13:51:49 -0500

    ...

--- game/character_menu.gd
+++ game/character_menu.gd
@@ -4,6 +4,12 @@ func _ready():
 	_initialize()
 	Global.player_changed.connect(func(player): _initialize())
 	Global.game_loaded.connect(func(): _initialize())
+	
+	%CloseButton.pressed.connect(
+		func():
+			print('close')
+			visible = false
+	)
 
 func _initialize():
 	if %Inventory:
