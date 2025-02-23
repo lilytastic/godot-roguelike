ca1211f6c3ead08ace0407213e60258f91c272ef
Lily <thebeaul@gmail.com>
2025-02-23 17:26:53 -0500

    ...

--- game/item_preview.gd
+++ game/item_preview.gd
@@ -1,4 +1,4 @@
-extends Panel
+extends VBoxContainer
 
 var _item = ''
 @export var item: String = '':
@@ -24,11 +24,17 @@ func _ready():
 	_update()
 
 func _update():
-	if !entity:
+	if !is_instance_valid(entity):
 		visible = false
 		return
 	visible = true
 	%ItemIcon.texture = entity.glyph.to_atlas_texture()
 	%ItemIcon.modulate = entity.glyph.fg
-	%ItemName.text = entity.blueprint.name.capitalize()
+	if is_instance_valid(entity.blueprint):
+		%ItemName.text = entity.blueprint.name.capitalize()
+		%ItemType.text = entity.blueprint.type.capitalize()
+		%Description.text = entity.blueprint.description
+		if entity.blueprint.weapon:
+			%WeaponDamage.text = str(entity.blueprint.weapon.damage[0]) + '-' + str(entity.blueprint.weapon.damage[1]) + ' Damage'
+			%WeaponSpeed.text = 'x' + str(entity.blueprint.weapon.speed) + ' Attack Speed'
 	print(entity)
