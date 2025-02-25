df3c2bed98e94191a33805f1433f9d20c0551795
Lily <thebeaul@gmail.com>
2025-02-24 21:40:17 -0500

    wheeeeee

---
+++ data/skill.gd
@@ -0,0 +1,6 @@
+class_name Skill
+extends Resource
+
+@export var name: String
+@export var description: String
+@export_enum("Combat", "Strength", "Dexterity", "Mind", "Arcane", "Deemonic") var skill_name: String
