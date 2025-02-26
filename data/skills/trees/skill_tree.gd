42004d994dea1249e0192a7c5f750b6cb96a1f62
Lily <thebeaul@gmail.com>
2025-02-26 17:22:31 -0500

    Add a ton of skills

--- data/skills/trees/skill_tree.gd
+++ data/skills/trees/skill_tree.gd
@@ -3,4 +3,5 @@ extends Resource
 
 @export var name: String
 @export var description: String
+@export var is_visible: bool
 @export var sort_order: int
--- game/skill_tree_select.gd
+++ game/skill_tree_select.gd
@@ -5,9 +5,7 @@ var skill_trees := []
 func _init():
 	clear_tabs()
 	
-	skill_trees = AgentManager.skill_trees.values()
+	skill_trees = AgentManager.skill_trees.values().filter(func(tree): return tree.is_visible)
 	skill_trees.sort_custom(func(a, b): return a.sort_order < b.sort_order)
 	for tree in skill_trees:
 		add_tab(tree.name)
-
-	print('trees: ', skill_trees)
